b77e6c748545ee399f35677331d26ac987ba5fd0
Diego Ramos <diego.ramos@imagemaker.com>
2021-03-27 23:11:05 -0300

    Add Protected Routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
+/* eslint-disable react/jsx-closing-tag-location */
 import React from 'react'
 import { Logo } from './components/Logo'
 import { Home } from './pages/Home'
 import { GlobalStyle } from './styles/GlobalStyles'
 import { Router } from '@reach/router'
 import { Detail } from './pages/Detail'
+import { Favs } from './pages/Favs'
+import { User } from './pages/User'
+import { NotRegisteredUser } from './pages/NotRegisteredUser'
 import { NavBar } from './components/NavBar'
+
+const UserLogged = ({ children }) => {
+  return children({ isAuth: false })
+}
 
 export const App = () => {
   return (
@@ -16,6 +24,20 @@ export const App = () => {
         <Home path='/pet/:categoryId' />
         <Detail path='/detail/:detailId' />
       </Router>
+      <UserLogged>
+        {
+            ({ isAuth }) =>
+              isAuth
+                ? <Router>
+                  <Favs path='/favs' />
+                  <User path='/user' />
+                </Router>
+                : <Router>
+                  <NotRegisteredUser path='/favs' />
+                  <NotRegisteredUser path='/user' />
+                </Router>
+          }
+      </UserLogged>
       <NavBar />
     </div>
   )
